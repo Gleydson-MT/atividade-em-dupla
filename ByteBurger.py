31324a63c4c95fcd9f0b707b16250fb1c681ab68
Gleydson-MT <275184323+Gleydson-MT@users.noreply.github.com>
2026-05-12 19:04:11 -0300

    ok

--- ByteBurger.py
+++ ByteBurger.py
@@ -1,8 +1,9 @@
 itens  = ["X-Burguer", "X-Salada", "Fritas", "Refrigerante", "Suco", "Sorvete"]
 precos = [18.50,        21.00,      9.00,     6.50,           7.00,   8.00     ]
 
-cliente_do_dia = []
+clientes = []
 faturamento_dia = 0
+todos_itens_vendidos = []
 # Aqui o foco é imprimir primeiro o cardápio.
 print(f"    ======CARDAPIO======     ")
 for i, iten in enumerate(itens):
@@ -10,20 +11,39 @@ for i, iten in enumerate(itens):
 print()
 
 #Aqui a ideia é armazenar o pedido do cliente dentro dessa repetição.
-pedido_atual = []
-preco_cliente = []
+
 while True:
     print(f"Digite (fim), para encerrar o atendimento.")
     cliente = (input("Digite o nome do cliente: "))
-    cliente_do_dia.append
+    
     if cliente == "fim":
         break
     else:
-       print(f"Bem vindo ao ByteBurguer\n Pedido em contrução...\n: ")
+        print(f"Bem vindo ao ByteBurguer\n Pedido em contrução...\n: ")
+        pedido_atual = []
+        preco_cliente_atual = []
     while True:
         resposta = (input("Digite o numero que repreta o produto do cliente\n (DIGITE 0 PARA SAIR): "))
         if resposta == "0":
             break
         else:
-            resposta = preco_cliente
-            print(preco_cliente)
+            resposta = int(resposta)
+            indice = resposta -1
+            pedido_atual.append(itens[indice])
+            preco_cliente_atual.append(precos[indice])
+            todos_itens_vendidos.append(precos[indice])
+            print(f"{itens[indice]} adicionado ao pedido!")
+#Bônus - Remover intem
+
+if len(pedido_atual) >0:
+    remover = input("\nDeseja remover algum intem? (s/n): ")
+    if remover.lower() == "s":
+        print("\n==== ITENS DO PEDIDO ====")
+
+        for i, iten in enumerate(pedido_atual):
+            print (f"{i+1} - {iten} ...... R$:{precos[i]:.2f}")
+        posicao = int(input("Digite o número do item para remover: "))
+        pedido_atual.pop(posicao -1)
+        preco_cliente_atual(posicao -1)
+
+        print("Itrm removido com sucesso!")
